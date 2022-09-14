ecc50b19f81d5989556db9ad9320137710aaf2bb
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-09-14 10:04:38 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px","");
+    const right = parseInt(rightNumbers,10);
+    if (right > 0) {
+        dodger.style.left=`${right + 1}px`;
+    }
+  }
+  document.addEventListener("keydown",function(event){
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  })
